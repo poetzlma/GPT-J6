59dfa01972edb4cf36d66c7aef58f74bb710cd63
maximilian <maximilian.poetzl@pwc.com>
2022-06-09 13:42:41 +0000

    Added  ipython notbook

--- cuda_torch_new.sh
+++ cuda_torch_new.sh
@@ -33,7 +33,7 @@ nvcc --version
 
 # install pytorch and transformers
 
-#next time use venev
+# next time use venev
 
 sudo apt install python3-pip
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
